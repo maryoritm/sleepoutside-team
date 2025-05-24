e5d487ae457ed88f0f72d24259ade9ff6b176074
maryoritm <maryor.trrs@gmail.com>
2025-05-24 13:23:24 -0600

    W03

--- my-team-project/js/main.js
+++ my-team-project/js/main.js
@@ -1,7 +1,11 @@
-import ProductData from './ProductData.mjs';
-import ProductList from './ProductList.mjs';
+import { loadHeaderFooter } from "./utils.mjs";
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
 
-const dataSource = new ProductData('tents');
-const listElement = document.querySelector('.product-list');
-const productList = new ProductList('tents', dataSource, listElement);
+loadHeaderFooter();
+
+const dataSource = new ProductData("tents");
+const element = document.querySelector(".product-list");
+const productList = new ProductList("Tents", dataSource, element);
+
 productList.init();
